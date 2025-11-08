5667ddbef4e7e55d4916a7f765e4951066b1b713
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2025-11-08 23:32:29 +0530

    Update 1solidrectangle.c

--- pattern_printing/1solidrectangle.c
+++ pattern_printing/1solidrectangle.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("Enter a number of rows:- ");
+    scanf("%d",&n);
+    printf("Enter a number of columns:- ");
+    scanf("%d",&m);
+    for(int i=1;i<=n;i++){     // outerloop -> no. of lines
+        for(int i=1;i<=m;i++){ // inner loop -> no. of stars in each line
+        printf(" * ");
+        }
+        printf("\n");    // har line ke baad ek enter maarne ke liye hai
+    }
+    return 0;
+}
