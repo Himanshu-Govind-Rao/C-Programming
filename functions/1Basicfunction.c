a35ace3a4208973257b4545218a9f329d85df31a
Himanshu-Govind-Rao <himanshugovindrao13@gmail.com>
2025-11-08 23:29:42 +0530

    Update 1Basicfunction.c

--- functions/1Basicfunction.c
+++ functions/1Basicfunction.c
@@ -1,1 +1,29 @@
+ #include<stdio.h>
+ int main(){
+  void india();
+      india();
+    return 0;
+   }
+   void india(){ 
+        printf("you are in India\n");
+        void australia();
+        australia();
+    return;
+   }
+   void australia(){ 
+        printf("you are in Australia\n");
+        void england();
+        england();
+    return;
+ }
+   
+ void england(){ 
+        printf("you are in England\n");
+    return;
+ }
+ 
+   
+  
+
+   
 
